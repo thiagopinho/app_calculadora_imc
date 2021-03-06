019c950b88fb22217e0f01dddb008ae9dbcdcd37
Thiago Pinho <thiagopinho05@gmail.com>
2021-03-06 14:20:12 -0300

    Correção de erro referente aos valores do cálculo e mudança dascores do app

--- lib/main.dart
+++ lib/main.dart
@@ -14,113 +14,121 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   TextEditingController pesoController = TextEditingController();
   TextEditingController alturaController = TextEditingController();
-  
+
   GlobalKey<FormState> _chave = GlobalKey<FormState>();
 
-  String _info = "Informe os seus dados";
+  String _infoText = "Informe os seus dados";
 
   void _reset() {
     pesoController.text = "";
     alturaController.text = "";
     setState(() {
-           _info = "Informe seus dados";
-        });
+      _infoText = "Informe seus dados";
+    });
   }
 
   void _calcular() {
-    double peso = double.parse(pesoController.text);
-    double altura = double.parse(alturaController.text) / 100;
-
-    double imc =  peso / (altura * altura);
-    
     setState(() {
-          if (imc < 18.5) {​​​​​
-_info = "Abaixo do peso (${​​​​​imc.toStringAsPrecision(3)}​​​​​)";
-}​​​​​ else if (imc >= 18.5 && imc <= 24.9) {​​​​​
-_info = "Peso normal(${​​​​​imc.toStringAsPrecision(3)}​​​​​)";
-}​​​​​ else if (imc >= 25.0 && imc <= 29.9) {​​​​​
-_info = "Sobrepeso(${​​​​​imc.toStringAsPrecision(3)}​​​​​)";
-}​​​​​ else if (imc >= 30.0 && imc <= 34.9) {​​​​​
-_info = "Obesidade grau 1 (${​​​​​imc.toStringAsPrecision(3)}​​​​​)";
-}​​​​​ else {​​​​​
-_info = "Obesidade grau 2 (${​​​​​imc.toStringAsPrecision(3)}​​​​​)";
-}​​​​​
-        });
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text) / 100;
+      double imc = peso / (altura * altura);
+
+      if (imc < 18.6) {
+        _infoText = "Abaixo de Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 40.0) {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.purple,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: _reset,
-          )
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-       padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-         child: Form(
-           key: _chave,
-         child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Icon(
-                Icons.person_outline,
-                size: 100.0,
-                color: Colors.purple,
-              ),
-              TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: "Peso(kg)",
-                      labelStyle: TextStyle(color: Colors.black38)),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.purple, fontSize: 25.0),
-                  controller: pesoController),
-                  validator: (value){
-                      if(value.isEmpty){
+        appBar: AppBar(
+          title: Text("Calculadora IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: _reset,
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Form(
+              key: _chave,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(
+                    Icons.person_outline,
+                    size: 100.0,
+                    color: Colors.amber,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Peso(kg)",
+                        labelStyle: TextStyle(color: Colors.black38)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    controller: pesoController,
+                    validator: (value) {
+                      if (value.isEmpty) {
                         return "Insira seu peso";
                       }
-                  },
-              TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: "Altura(cm)",
-                      labelStyle: TextStyle(color: Colors.black38)),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.purple, fontSize: 25.0),
-                  controller: alturaController,
-                  validator: (value){
-                      if(value.isEmpty){
+                    },
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Altura(cm)",
+                        labelStyle: TextStyle(color: Colors.black38)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    controller: alturaController,
+                    validator: (value) {
+                      if (value.isEmpty) {
                         return "Insira sua altura";
                       }
-                  },
+                    },
                   ),
-                  
-              Container(
-                margin:
-                    EdgeInsets.only(left: 30, right: 30, top: 20, bottom: 20),
-                height: 60.0,
-                child: ElevatedButton(
-                  onPressed: _calcular,
-                  child: Text("Calcular",
-                      style: TextStyle(color: Colors.white, fontSize: 25.0)),
-                  // color: Colors.purple,
-                ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0, bottom: 20.0),
+                    child: Container(
+                      height: 60.0,
+                      child: RaisedButton(
+                        onPressed: () {
+                          if (_chave.currentState.validate()) {
+                            _calcular();
+                          }
+                        },
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                        color: Colors.amber,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    _infoText,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 28.0),
+                  )
+                ],
               ),
-              Text(
-                _info,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.green, fontSize: 28.0))
-            ]),
-      ),
-      ),
-    );
+            )));
   }
 }
